9e21f5a1f83e818bb259e22c6ba3b5a6a4ea47cf
Arsen <arsenparsen1970@mail.ru>
2021-05-21 19:36:53 +0300

    Поправил еще раз!

--- CSharpArsenManukjanSnake/HorizontalLine.cs
+++ CSharpArsenManukjanSnake/HorizontalLine.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpArsenManukjanSnake;
+
+namespace CSharpArsenManukjanSnake
+{
+    class HorizontalLine : Figure
+    {
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
