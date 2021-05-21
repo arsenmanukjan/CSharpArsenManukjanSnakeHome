ec47f862ff8d26823492f38a9fdff047cb312bcf
Arsen <arsenparsen1970@mail.ru>
2021-05-21 19:21:39 +0300

    Урок 9 ,21.05.2021, время 19:18 enum(direction), snake.

---
+++ CSharpArsenManukjanSnake/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpArsenManukjanSnake
+{
+    class Figure : Point
+    {
+        protected List<Point> pList;
+
+        public new void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- CSharpArsenManukjanSnake/Program.cs
+++ CSharpArsenManukjanSnake/Program.cs
@@ -10,12 +10,38 @@ namespace CSharpArsenManukjanSnake
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
+=======
+            Console.SetWindowSize(80, 25);  // пришлось добавить, потому что не запускалось с SetBufferSize
+            Console.SetBufferSize(80, 25);  // проверка
+
+            // Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+>>>>>>> Stashed changes
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
             
+<<<<<<< Updated upstream
+=======
+
+
+            // Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake( p, 4, Direction.RIGHT);
+            snake.Draw();
+>>>>>>> Stashed changes
             Console.ReadLine();
         }
     }
