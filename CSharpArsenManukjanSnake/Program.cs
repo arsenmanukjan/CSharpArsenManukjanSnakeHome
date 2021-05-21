bdf224588cc30d0a184492e01a655641a087fdd0
Arsen <arsenparsen1970@mail.ru>
2021-05-21 16:44:20 +0300

    Создали класс горизонтальная линия, урок 7, (16:42), 21.05.2021. Добавили плюсики в горизонталь

--- CSharpArsenManukjanSnake/Program.cs
+++ CSharpArsenManukjanSnake/Program.cs
@@ -15,7 +15,10 @@ namespace CSharpArsenManukjanSnake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
+
             Console.ReadLine();
         }
     }
